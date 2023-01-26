a7d0af8c7b44db26d9acea3aa9f254b4fb3a158e
kasiwamotchi <totopomu@gamil.com>
2023-01-26 19:14:10 +0900

    Inputタグを設定

--- ToDoList-React.jsx
+++ ToDoList-React.jsx
@@ -35,17 +35,28 @@ const TodoList = () => {
                 //やってることは2つ上と同じ
             },
         ]
+
+        const [task, setTask] = useState('')
+        //定数Taskの中のSetTaskに対して空白を保持する
+        //フック機能ってなんだよ。今回はリアクトのフック機能でsTATE関数をつかっているらしい。Taskはもっている状態をもってくるものらしい。
+
+        Add Task : <input value={ task } placeholder="Add New Task" onChange={handleNewTask}/>
+        //変数Add taskを定義。初期値をTaskに設定するInputタグ
+
         const [todos, setTodos] = useState(initialState);
         //定数todos,とsetTodosは引数をinitialStateにしてuseStateという関数を行う
         return (
             <div>
                 <h1>ToDo List</h1>
+                //HTML要素のh1要素でToDo Listを出力する
+                Add Task : <input placeholder="Add New Task" />
+                //変数AddTaskをAddNewTaskと書かれたPlaceholder（入力前に出てくる文字）属性を持つインプットタグを持たせる
                 <ul>
                 { todos.map((todo, index) => (
                     <li key={ index }>{ todo.task }</li>
                 ))}
                 </ul>
             </div>
-            //HTML要素のh1要素でToDo Listを出力する
+            //todosにmap関数を用いてTodo中のTask要素を引数にしてLi要素を作っている。KeyはReactに使う識別子のようなもの直接的な意味はないらしい？
 );
 }
